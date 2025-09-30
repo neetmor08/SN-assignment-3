67c051b76952d49f87759808611288f78b663883
neetmor08 <neetmor08@gmail.com>
2025-09-30 10:17:29 +1000

    Add files via upload

---
+++ decorators.py
@@ -0,0 +1,18 @@
+﻿from functools import wraps
+from tkinter import messagebox
+
+def log_action(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"[LOG] {func.__name__} called")
+        return func(*args, **kwargs)
+    return wrapper
+
+def handle_errors(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as exc:
+            messagebox.showerror("Error", str(exc))
+    return wrapper
